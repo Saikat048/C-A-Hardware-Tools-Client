131cdef5fe93678880d9b233135a3a5fb4924c6b
Saikat048 <mdsaikathosan048@gmail.com>
2022-05-21 23:22:42 +0600

    commit login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css'; 
-import Navbar from './Shared/Navbar'
+import './App.css';  
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home'
+import Navbar from './Pages/Shared/Navbar';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Login/Signup';
 
 function App() {
   return (
     <div> 
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+      </Routes>
     </div>
   );
 }
